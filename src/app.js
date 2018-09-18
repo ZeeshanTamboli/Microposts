4a85df6173302408c140cf12852615f3a8d226f8
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-09-18 21:39:11 +0530

    Delete posts

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ document.addEventListener('DOMContentLoaded', getPosts);
 //Event listener for add posts
 document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+// Listen for delete
+document.querySelector('#posts').addEventListener('click', deletePost);
+
 async function getPosts() {
   try {
     const posts = await http.get('http://localhost:3000/posts');
@@ -25,12 +28,28 @@ async function submitPost() {
     const data = {
       title,
       body
-    }
+    };
 
     await http.post('http://localhost:3000/posts', data);
     ui.showAlert('Post added', 'alert alert-success');
     ui.clearFields();
     getPosts();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Delete post
+async function deletePost(e) {
+  try {
+    if (e.target.parentElement.classList.contains('delete')) {
+      const id = e.target.parentElement.dataset.id;
+      if (confirm('Are you sure?')) {
+        const data = await http.delete(`http://localhost:3000/posts/${id}`);
+        ui.showAlert('Post Removed!', 'alert alert-success');
+        getPosts();
+      }
+    }
   } catch (error) {
     console.log(error);
   }
