583a794007072466e8f7d739eaab1bebe04bd533
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-09-19 22:55:22 +0530

    Update posts

--- src/app.js
+++ src/app.js
@@ -13,6 +13,9 @@ document.querySelector('#posts').addEventListener('click', deletePost);
 // Listen for edit state
 document.querySelector('#posts').addEventListener('click', enableEdit);
 
+// Listen for cancel
+document.querySelector('.card-form').addEventListener('click', cancelEdit);
+
 async function getPosts() {
   try {
     const posts = await http.get('http://localhost:3000/posts');
@@ -27,16 +30,31 @@ async function submitPost() {
   try {
     const title = document.querySelector('#title').value;
     const body = document.querySelector('#body').value;
+    const id = document.querySelector('#id').value;
 
     const data = {
       title,
       body
     };
 
-    await http.post('http://localhost:3000/posts', data);
-    ui.showAlert('Post added', 'alert alert-success');
-    ui.clearFields();
-    getPosts();
+    // Validate input
+    if(title === '' || body === '') {
+      ui.showAlert('Please fill in all fields', 'alert alert-danger');
+    } else {
+      if(id === '') {
+        // Create post
+        await http.post('http://localhost:3000/posts', data);
+        ui.showAlert('Post added', 'alert alert-success');
+        ui.clearFields();
+        getPosts();
+      } else {
+        // Update post
+        await http.put(`http://localhost:3000/posts/${id}`, data);
+        ui.showAlert('Post updated', 'alert alert-success');
+        ui.changeFormState('add');
+        getPosts();
+      }
+    }  
   } catch (error) {
     console.log(error);
   }
@@ -78,4 +96,12 @@ async function enableEdit(e) {
     }
     e.preventDefault();
   } catch (error) {}
+}
+
+function cancelEdit(e) {
+  if(e.target.classList.contains('post-cancel')) {
+    ui.changeFormState('add');
+  }
+
+  e.preventDefault();
 }
--- src/ui.js
+++ src/ui.js
@@ -15,9 +15,7 @@ class UI {
       htmlContent += `
         <div class="card mb-3">
           <div class="card-body">
-            <h4 class="card-title">
-              ${post.title}
-            </h4>
+          <h4 class="card-title">${post.title}</h4>
             <p class="card-text">${post.body}</p>
             <a href="#" class="edit card-link" data-id="${post.id}">
               <i class="fa fa-pencil"></i>
@@ -72,6 +70,45 @@ class UI {
     this.titleInput.value = data.title;
     this.bodyInput.value = data.body;
     this.idInput.value = data.id;
+
+    this.changeFormState('edit');
+  }
+
+  // Change the form state
+  changeFormState(type) {
+    if(type === 'edit') {
+      this.postSubmit.textContent = 'Update Post';
+      this.postSubmit.className = 'post-submit btn btn-warning btn-block';
+
+      // Create cancel button
+      const button = document.createElement('button');
+      button.className = 'post-cancel btn btn-light btn-block';
+      button.appendChild(document.createTextNode('Cancel Edit'));
+
+      // Get parent
+      const cardForm = document.querySelector('.card-form');
+      // Get element to insert before
+      const formEnd = document.querySelector('.form-end');
+      // Insert before
+      cardForm.insertBefore(button, formEnd);
+    } else {
+      this.postSubmit.textContent = 'Post It';
+      this.postSubmit.className = 'post-submit btn btn-primary btn-block';
+
+      // If cancel button is present, remove it
+      if (document.querySelector('.post-cancel')) {
+        document.querySelector('.post-cancel').remove();
+      }
+
+      // Clear ID from hidden field
+      this.clearIdInput();
+      // Clear text
+      this.clearFields();
+    }
+  }
+
+  clearIdInput() {
+    this.idInput.value = '';
   }
 }
 
